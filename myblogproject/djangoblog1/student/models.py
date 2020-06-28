5cb0d236a943278ef87aebbcf3e28108c38fcaf9
zakirp4 <zakirp4@gmail.com>
2020-06-28 08:23:55 +0600

    class 3 with migration and student

--- myblogproject/djangoblog1/student/models.py
+++ myblogproject/djangoblog1/student/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class StudentList(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    roll = models.IntegerField()
+    gender = models.CharField(max_length=6)
+
